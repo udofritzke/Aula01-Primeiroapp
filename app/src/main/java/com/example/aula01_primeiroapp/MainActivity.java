bd84de625bb8e5002c33cd7cda700825c4d84e81
udofritzke <udo.fritzke@gmail.com>
2023-03-14 19:10:48 -0300

    Atividade com dois botões e uma área de texto. Agora com métodos para tratar os botões.

--- app/src/main/java/com/example/aula01_primeiroapp/MainActivity.java
+++ app/src/main/java/com/example/aula01_primeiroapp/MainActivity.java
@@ -3,14 +3,39 @@ package com.example.aula01_primeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 /*
 Repositório GitHub: https://github.com/udofritzke/Aula01-Primeiroapp
  */
 public class MainActivity extends AppCompatActivity {
-
+    private Button botaoApaga;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+           Tratador do evento do botão associado por uma classe anônima
+        */
+        botaoApaga = (Button) findViewById(R.id.botaoApaga);
+        botaoApaga.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view){
+                TextView texto = (TextView) findViewById(R.id.textView);
+                texto.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+    /*
+    Tratador do evento do botão associado em "activity_main.xml"
+     */
+    public void botaoEscreve(View view) {
+        TextView texto = (TextView) findViewById(R.id.textView);
+        texto.setVisibility(View.VISIBLE);
+        texto.setText("Nova mensagem de texto");
     }
 }
